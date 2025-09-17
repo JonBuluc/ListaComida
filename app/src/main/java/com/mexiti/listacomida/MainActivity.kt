4acea9994d913a14d12560ad61b2d2b20b7d99c7
JonBuluc <jonanb515@gmail.com>
2025-09-16 20:33:08 -0600

    Se hizo la tarea 4

--- app/src/main/java/com/mexiti/listacomida/MainActivity.kt
+++ app/src/main/java/com/mexiti/listacomida/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +16,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,7 +34,12 @@ import androidx.compose.ui.unit.dp
 import com.mexiti.listacomida.data.DataSource
 import com.mexiti.listacomida.model.Platillo
 import com.mexiti.listacomida.ui.theme.ListaComidaTheme
-import com.mexiti.listacomida.ui.theme.md_theme_dark_onSecondary
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +58,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+//Se agrego con el Scaffold
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuApp(){
     MenuCardList(
@@ -56,64 +67,119 @@ fun MenuApp(){
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuCardList( platilloList:List<Platillo>, modifier: Modifier = Modifier ){
-    LazyColumn( modifier = modifier ){
-        items(platilloList){
-            platillo -> MenuCard(
-            platillo = platillo,
-                modifier= Modifier.padding(10.dp)
-        )
+    Scaffold (
+        topBar = {
+            MenuTopAppBar()
         }
+    ){it ->
+        LazyColumn( contentPadding = it ){
+            items(platilloList){
+                platillo -> MenuCard(
+                platillo = platillo,
+                    modifier= Modifier.padding(10.dp)
+            )
+            }
 
+        }
     }
 }
 
 
 
 @Composable
-fun MenuCard(platillo: Platillo, modifier: Modifier = Modifier){
-    Card(modifier = modifier
-        .padding(10.dp)) {
-        Row (
+fun MenuCard(platillo: Platillo, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .padding(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(10.dp),
             verticalAlignment = Alignment.CenterVertically
-        )  {
+        ) {
             Image(
                 painter = painterResource(id = platillo.drawableResourceId),
                 contentDescription = stringResource(id = platillo.stringResourceId),
                 modifier = Modifier
                     .size(180.dp)
-                    .padding(start = 10.dp)
-                    .clip(CircleShape)
-                ,
+                    .clip(CircleShape),
                 contentScale = ContentScale.Crop
             )
+
             Column(
-                modifier =
-                    Modifier.padding(start = 20.dp)
+                modifier = Modifier.padding(start = 20.dp)
             ) {
+
                 Text(
                     text = "Pozole",
-                    style = MaterialTheme.typography.headlineSmall
-
+                    //Use unas letras mas pequeñas pero igual cambie
+                    //el tamaño con el dp
+                    style = MaterialTheme.typography.headlineSmall.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
                 )
-                Text(text = "MX $100.0 ")
+
+                Text(
+                    text = "MX $100",
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.Medium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                )
+
                 Text(
                     text = "Ahorra hasta el 30%",
-                    fontWeight = FontWeight.Bold,
-                    color = md_theme_dark_onSecondary
-                    
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
                 )
             }
-
-
         }
-
     }
+}
 
+//Esta parte la agregue con ayuda de gemini
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .size(65.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color(0xFF82B1FF))
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo_commudel),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape)
+                    )
+                }
 
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Commudel",
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
@@ -121,5 +187,12 @@ fun MenuCard(platillo: Platillo, modifier: Modifier = Modifier){
 fun MenuPlatilloPreview() {
     ListaComidaTheme(darkTheme = false) {
         MenuCard(platillo = Platillo(R.string.pozole,R.drawable.pozole) )
+    }
+}
+@Preview
+@Composable
+fun MenuTopAppBarPreview(){
+    ListaComidaTheme {
+        MenuTopAppBar()
     }
 }
--- app/src/main/java/com/mexiti/listacomida/ui/theme/Type.kt
+++ app/src/main/java/com/mexiti/listacomida/ui/theme/Type.kt
@@ -2,22 +2,56 @@ package com.mexiti.listacomida.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mexiti.listacomida.R // Import your project's R file
+
+val Fredoka = FontFamily(
+    Font(R.font.fredoka)
+)
+val Nunito = FontFamily(
+    Font(R.font.nunito)
+)
+val ComicNeue = FontFamily(
+    Font(R.font.comicneue_bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Fredoka,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Bold,
+        fontSize = 25.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Nunito,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = ComicNeue,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Fredoka, // You can also apply it to other styles
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
